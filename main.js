9352c48cd3a3c35f7002410cee2c2a284f99d7bc
Vladimir Penkin <Vladimir.Penkin@kyivstar.ua>
2020-02-21 15:36:10 +0200

    добавлены комментарии

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
-
-function nextElement () {
-
-	let elem = document.getElementById("myId");
-	elem.style.color = "black";
-	elem.nextSibling.id = "myId";
-	elem.id = "newId";
-	document.getElementById("myId").style.color = "green";
-};
-
-function beforElement () {
-
-	let elem = document.getElementById("myId");
-	elem.style.color = "black";
-	elem.previousSibling.id = "green";
-	elem.id = "newId";
-	document.getElementById("myId").style.color = "green";
-
-};
-
+// выбираем первый элемент
 function changeFirstElement () {
-
 	let elem = document.getElementsByTagName("li");
 	elem[0].style.color = "green";
 	elem[0].id = "myId"
@@ -27,21 +7,37 @@ function changeFirstElement () {
 	elem[elem.length - 1].style.color = "black";
 };
 
+// выбираем последний элемент
 function changeLastElement () {
-
 	let elem = document.getElementsByTagName("li");
 	elem[elem.length - 1].style.color = "green";
 	elem[elem.length - 1].id = "myId";
 	elem[0].id = "newId";
 	elem[0].style.color = "black";
-
 };
 
-function addLastElement(form) {
+// выбираем следущий элемент
+function nextElement () {
+	let elem = document.getElementById("myId");
+	elem.style.color = "black";
+	elem.nextSibling.id = "myId";
+	elem.id = "newId";
+	document.getElementById("myId").style.color = "green";
+};
 
+// выбираем предыдущий элемент
+function beforElement () {
+	let elem = document.getElementById("myId");
+	elem.style.color = "black";
+	elem.previousSibling.id = "green";
+	elem.id = "newId";
+	document.getElementById("myId").style.color = "green";
+};
+
+//добавляем последний элемент
+function addLastElement(form) {
 	let el = document.getElementById("myId");
 	el.id = "newId";
-
 	let newElement = document.createElement("li");
 	newElement.id = "myId";
 	newElement.innerHTML = "Последний элемент";
@@ -50,32 +46,29 @@ function addLastElement(form) {
 	elem[0].id = "newId";
 };
 
+//удаляем последний элемент
 function removeLastElement(form) {
-
 	let mainObj = document.getElementById("myList");
 	let delLi = mainObj.lastChild;
 	mainObj.removeChild(delLi);
 	let li = document.getElementsByTagName("li");
 	li[li.length - 1].id ="myId";
-
 };
 
+//добавляем первый элемент
 function addFirstElement(form) {
-
 	let newElement = document.createElement("li");
 	newElement.id = "myId";
 	newElement.innerHTML = "Нулевой элемент";
 	myList.prepend(newElement);
-
 };
 
+//удаляем первый элемент
 function removeFirstElement(form) {
-
 	let mainObj = document.getElementById("myList");
 	let delLi = mainObj.firstElementChild;
 	mainObj.removeChild(delLi);
 	let li = document.getElementsByTagName("li");
 	li[0].id = "myId";
-
 };
 
